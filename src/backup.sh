d54125503bb7ce1579a0287d7817dda42c9ce2dc
Jan Brinkmann <jan.brink@openpi.de>
2021-11-08 21:10:41 +0100

    Remove huge if block.

--- src/backup.sh
+++ src/backup.sh
@@ -10,148 +10,145 @@ function info {
 }
 
 if [ "$CHECK_HOST" != "false" ]; then
+  info "Check host availability"
   TEMPFILE="$(mktemp)"
   ping -c 1 $CHECK_HOST | grep '1 packets transmitted, 1 received' > "$TEMPFILE"
   PING_RESULT="$(cat $TEMPFILE)"
   if [ ! -z "$PING_RESULT" ]; then
-    skip="false"
     echo "$CHECK_HOST is available."
   else
-    skip="true"
     echo "$CHECK_HOST is not available."
     info "Backup skipped"
+    exit 0
   fi
-else
-  skip="false"
 fi
 
-if [ "$skip" == "false" ]; then
-  info "Backup starting"
-  TIME_START="$(date +%s.%N)"
-  DOCKER_SOCK="/var/run/docker.sock"
+info "Backup starting"
+TIME_START="$(date +%s.%N)"
+DOCKER_SOCK="/var/run/docker.sock"
 
-  if [ ! -z "$BACKUP_CUSTOM_LABEL" ]; then
-    CUSTOM_LABEL="--filter label=$BACKUP_CUSTOM_LABEL"
+if [ ! -z "$BACKUP_CUSTOM_LABEL" ]; then
+  CUSTOM_LABEL="--filter label=$BACKUP_CUSTOM_LABEL"
+fi
+
+if [ -S "$DOCKER_SOCK" ]; then
+  TEMPFILE="$(mktemp)"
+  docker ps --format "{{.ID}}" --filter "label=docker-volume-backup.stop-during-backup=true" $CUSTOM_LABEL > "$TEMPFILE"
+  CONTAINERS_TO_STOP="$(cat $TEMPFILE | tr '\n' ' ')"
+  CONTAINERS_TO_STOP_TOTAL="$(cat $TEMPFILE | wc -l)"
+  CONTAINERS_TOTAL="$(docker ps --format "{{.ID}}" | wc -l)"
+  rm "$TEMPFILE"
+  echo "$CONTAINERS_TOTAL containers running on host in total"
+  echo "$CONTAINERS_TO_STOP_TOTAL containers marked to be stopped during backup"
+else
+  CONTAINERS_TO_STOP_TOTAL="0"
+  CONTAINERS_TOTAL="0"
+  echo "Cannot access \"$DOCKER_SOCK\", won't look for containers to stop"
+fi
+
+if [ "$CONTAINERS_TO_STOP_TOTAL" != "0" ]; then
+  info "Stopping containers"
+  docker stop $CONTAINERS_TO_STOP
+fi
+
+if [ -S "$DOCKER_SOCK" ]; then
+  TEMPFILE="$(mktemp)"
+  docker ps \
+    --filter "label=docker-volume-backup.exec-pre-backup" $CUSTOM_LABEL \
+    --format '{{.ID}} {{.Label "docker-volume-backup.exec-pre-backup"}}' \
+    > "$TEMPFILE"
+  while read line; do
+    info "Pre-exec command: $line"
+    docker exec $line
+  done < "$TEMPFILE"
+  rm "$TEMPFILE"
+fi
+
+info "Creating backup"
+BACKUP_FILENAME="$(date +"${BACKUP_FILENAME:-backup-%Y-%m-%dT%H-%M-%S.tar.gz}")"
+TIME_BACK_UP="$(date +%s.%N)"
+tar -czvf "$BACKUP_FILENAME" $BACKUP_SOURCES # allow the var to expand, in case we have multiple sources
+BACKUP_SIZE="$(du --bytes $BACKUP_FILENAME | sed 's/\s.*$//')"
+TIME_BACKED_UP="$(date +%s.%N)"
+
+if [ ! -z "$GPG_PASSPHRASE" ]; then
+  info "Encrypting backup"
+  gpg --symmetric --cipher-algo aes256 --batch --passphrase "$GPG_PASSPHRASE" -o "${BACKUP_FILENAME}.gpg" $BACKUP_FILENAME
+  rm $BACKUP_FILENAME
+  BACKUP_FILENAME="${BACKUP_FILENAME}.gpg"
+fi
+
+if [ ! -z "$SCP_HOST" ]; then
+  info "Uploading backup by means of SCP"
+  echo "Will upload to $SCP_HOST:$SCP_DIRECTORY"
+  TIME_UPLOAD="$(date +%s.%N)"
+  scp -ro StrictHostKeyChecking=no -i /ssh/id_rsa $BACKUP_FILENAME $SCP_USER@$SCP_HOST:$SCP_DIRECTORY
+  echo "Upload finished"
+  TIME_UPLOADED="$(date +%s.%N)"
+fi
+
+if [ -d "$BACKUP_ARCHIVE" ]; then
+  info "Archiving backup"
+  mv -v "$BACKUP_FILENAME" "$BACKUP_ARCHIVE/$BACKUP_FILENAME"
+  if (($BACKUP_UID > 0)); then
+    chown -v $BACKUP_UID:$BACKUP_GID "$BACKUP_ARCHIVE/$BACKUP_FILENAME"
+  fi
   fi
 
-  if [ -S "$DOCKER_SOCK" ]; then
-    TEMPFILE="$(mktemp)"
-    docker ps --format "{{.ID}}" --filter "label=docker-volume-backup.stop-during-backup=true" $CUSTOM_LABEL > "$TEMPFILE"
-    CONTAINERS_TO_STOP="$(cat $TEMPFILE | tr '\n' ' ')"
-    CONTAINERS_TO_STOP_TOTAL="$(cat $TEMPFILE | wc -l)"
-    CONTAINERS_TOTAL="$(docker ps --format "{{.ID}}" | wc -l)"
-    rm "$TEMPFILE"
-    echo "$CONTAINERS_TOTAL containers running on host in total"
-    echo "$CONTAINERS_TO_STOP_TOTAL containers marked to be stopped during backup"
-  else
-    CONTAINERS_TO_STOP_TOTAL="0"
-    CONTAINERS_TOTAL="0"
-    echo "Cannot access \"$DOCKER_SOCK\", won't look for containers to stop"
+if [ "$CONTAINERS_TO_STOP_TOTAL" != "0" ]; then
+  info "Starting containers back up"
+  docker start $CONTAINERS_TO_STOP
+fi
+
+info "Waiting before processing"
+echo "Sleeping $BACKUP_WAIT_SECONDS seconds..."
+sleep "$BACKUP_WAIT_SECONDS"
+
+TIME_UPLOAD="0"
+TIME_UPLOADED="0"
+if [ ! -z "$AWS_S3_BUCKET_NAME" ]; then
+  info "Uploading backup to S3"
+  echo "Will upload to bucket \"$AWS_S3_BUCKET_NAME\""
+  TIME_UPLOAD="$(date +%s.%N)"
+  aws $AWS_EXTRA_ARGS s3 cp --only-show-errors "$BACKUP_FILENAME" "s3://$AWS_S3_BUCKET_NAME/"
+  echo "Upload finished"
+  TIME_UPLOADED="$(date +%s.%N)"
+fi
+if [ ! -z "$AWS_GLACIER_VAULT_NAME" ]; then
+  info "Uploading backup to GLACIER"
+  echo "Will upload to vault \"$AWS_GLACIER_VAULT_NAME\""
+  TIME_UPLOAD="$(date +%s.%N)"
+  aws $AWS_EXTRA_ARGS glacier upload-archive --account-id - --vault-name "$AWS_GLACIER_VAULT_NAME" --body "$BACKUP_FILENAME"
+  echo "Upload finished"
+  TIME_UPLOADED="$(date +%s.%N)"
+fi
+
+if [ ! -z "$SCP_HOST" ]; then
+  info "Uploading backup by means of SCP"
+  echo "Will upload to $SCP_HOST:$SCP_DIRECTORY"
+  TIME_UPLOAD="$(date +%s.%N)"
+  scp -ro StrictHostKeyChecking=no -i /ssh/id_rsa $BACKUP_FILENAME $SCP_USER@$SCP_HOST:$SCP_DIRECTORY
+  echo "Upload finished"
+  TIME_UPLOADED="$(date +%s.%N)"
+fi
+
+if [ -d "$BACKUP_ARCHIVE" ]; then
+  info "Archiving backup"
+  mv -v "$BACKUP_FILENAME" "$BACKUP_ARCHIVE/$BACKUP_FILENAME"
+  if (($BACKUP_UID > 0)); then
+    chown -v $BACKUP_UID:$BACKUP_GID "$BACKUP_ARCHIVE/$BACKUP_FILENAME"
   fi
+fi
 
-  if [ "$CONTAINERS_TO_STOP_TOTAL" != "0" ]; then
-    info "Stopping containers"
-    docker stop $CONTAINERS_TO_STOP
-  fi
+if [ -f "$BACKUP_FILENAME" ]; then
+  info "Cleaning up"
+  rm -vf "$BACKUP_FILENAME"
+fi
 
-  if [ -S "$DOCKER_SOCK" ]; then
-    TEMPFILE="$(mktemp)"
-    docker ps \
-      --filter "label=docker-volume-backup.exec-pre-backup" $CUSTOM_LABEL \
-      --format '{{.ID}} {{.Label "docker-volume-backup.exec-pre-backup"}}' \
-      > "$TEMPFILE"
-    while read line; do
-      info "Pre-exec command: $line"
-      docker exec $line
-    done < "$TEMPFILE"
-    rm "$TEMPFILE"
-  fi
-
-  info "Creating backup"
-  BACKUP_FILENAME="$(date +"${BACKUP_FILENAME:-backup-%Y-%m-%dT%H-%M-%S.tar.gz}")"
-  TIME_BACK_UP="$(date +%s.%N)"
-  tar -czvf "$BACKUP_FILENAME" $BACKUP_SOURCES # allow the var to expand, in case we have multiple sources
-  BACKUP_SIZE="$(du --bytes $BACKUP_FILENAME | sed 's/\s.*$//')"
-  TIME_BACKED_UP="$(date +%s.%N)"
-
-  if [ ! -z "$GPG_PASSPHRASE" ]; then
-    info "Encrypting backup"
-    gpg --symmetric --cipher-algo aes256 --batch --passphrase "$GPG_PASSPHRASE" -o "${BACKUP_FILENAME}.gpg" $BACKUP_FILENAME
-    rm $BACKUP_FILENAME
-    BACKUP_FILENAME="${BACKUP_FILENAME}.gpg"
-  fi
-
-  if [ ! -z "$SCP_HOST" ]; then
-    info "Uploading backup by means of SCP"
-    echo "Will upload to $SCP_HOST:$SCP_DIRECTORY"
-    TIME_UPLOAD="$(date +%s.%N)"
-    scp -ro StrictHostKeyChecking=no -i /ssh/id_rsa $BACKUP_FILENAME $SCP_USER@$SCP_HOST:$SCP_DIRECTORY
-    echo "Upload finished"
-    TIME_UPLOADED="$(date +%s.%N)"
-  fi
-
-  if [ -d "$BACKUP_ARCHIVE" ]; then
-    info "Archiving backup"
-    mv -v "$BACKUP_FILENAME" "$BACKUP_ARCHIVE/$BACKUP_FILENAME"
-    if (($BACKUP_UID > 0)); then
-      chown -v $BACKUP_UID:$BACKUP_GID "$BACKUP_ARCHIVE/$BACKUP_FILENAME"
-    fi
-   fi
-
-  if [ "$CONTAINERS_TO_STOP_TOTAL" != "0" ]; then
-    info "Starting containers back up"
-    docker start $CONTAINERS_TO_STOP
-  fi
-
-  info "Waiting before processing"
-  echo "Sleeping $BACKUP_WAIT_SECONDS seconds..."
-  sleep "$BACKUP_WAIT_SECONDS"
-
-  TIME_UPLOAD="0"
-  TIME_UPLOADED="0"
-  if [ ! -z "$AWS_S3_BUCKET_NAME" ]; then
-    info "Uploading backup to S3"
-    echo "Will upload to bucket \"$AWS_S3_BUCKET_NAME\""
-    TIME_UPLOAD="$(date +%s.%N)"
-    aws $AWS_EXTRA_ARGS s3 cp --only-show-errors "$BACKUP_FILENAME" "s3://$AWS_S3_BUCKET_NAME/"
-    echo "Upload finished"
-    TIME_UPLOADED="$(date +%s.%N)"
-  fi
-  if [ ! -z "$AWS_GLACIER_VAULT_NAME" ]; then
-    info "Uploading backup to GLACIER"
-    echo "Will upload to vault \"$AWS_GLACIER_VAULT_NAME\""
-    TIME_UPLOAD="$(date +%s.%N)"
-    aws $AWS_EXTRA_ARGS glacier upload-archive --account-id - --vault-name "$AWS_GLACIER_VAULT_NAME" --body "$BACKUP_FILENAME"
-    echo "Upload finished"
-    TIME_UPLOADED="$(date +%s.%N)"
-  fi
-
-  if [ ! -z "$SCP_HOST" ]; then
-    info "Uploading backup by means of SCP"
-    echo "Will upload to $SCP_HOST:$SCP_DIRECTORY"
-    TIME_UPLOAD="$(date +%s.%N)"
-    scp -ro StrictHostKeyChecking=no -i /ssh/id_rsa $BACKUP_FILENAME $SCP_USER@$SCP_HOST:$SCP_DIRECTORY
-    echo "Upload finished"
-    TIME_UPLOADED="$(date +%s.%N)"
-  fi
-
-  if [ -d "$BACKUP_ARCHIVE" ]; then
-    info "Archiving backup"
-    mv -v "$BACKUP_FILENAME" "$BACKUP_ARCHIVE/$BACKUP_FILENAME"
-    if (($BACKUP_UID > 0)); then
-      chown -v $BACKUP_UID:$BACKUP_GID "$BACKUP_ARCHIVE/$BACKUP_FILENAME"
-    fi
-  fi
-
-  if [ -f "$BACKUP_FILENAME" ]; then
-    info "Cleaning up"
-    rm -vf "$BACKUP_FILENAME"
-  fi
-
-  info "Collecting metrics"
-  TIME_FINISH="$(date +%s.%N)"
-  INFLUX_LINE="$INFLUXDB_MEASUREMENT\
-  ,host=$BACKUP_HOSTNAME\
+info "Collecting metrics"
+TIME_FINISH="$(date +%s.%N)"
+INFLUX_LINE="$INFLUXDB_MEASUREMENT\
+,host=$BACKUP_HOSTNAME\
 \
 size_compressed_bytes=$BACKUP_SIZE\
 ,containers_total=$CONTAINERS_TOTAL\
@@ -161,19 +158,18 @@ size_compressed_bytes=$BACKUP_SIZE\
 ,time_compress=$(perl -E "say $TIME_BACKED_UP - $TIME_BACK_UP")\
 ,time_upload=$(perl -E "say $TIME_UPLOADED - $TIME_UPLOAD")\
 "
-  echo "$INFLUX_LINE" | sed 's/ /,/g' | tr , '\n'
+echo "$INFLUX_LINE" | sed 's/ /,/g' | tr , '\n'
 
-  if [ ! -z "$INFLUXDB_URL" ]; then
-    info "Shipping metrics"
-    curl \
-      --silent \
-      --include \
-      --request POST \
-      --user "$INFLUXDB_CREDENTIALS" \
-      "$INFLUXDB_URL/write?db=$INFLUXDB_DB" \
-      --data-binary "$INFLUX_LINE"
-  fi
-
-  info "Backup finished"
+if [ ! -z "$INFLUXDB_URL" ]; then
+  info "Shipping metrics"
+  curl \
+    --silent \
+    --include \
+    --request POST \
+    --user "$INFLUXDB_CREDENTIALS" \
+    "$INFLUXDB_URL/write?db=$INFLUXDB_DB" \
+    --data-binary "$INFLUX_LINE"
 fi
-  echo "Will wait for next scheduled backup"
+
+info "Backup finished"
+echo "Will wait for next scheduled backup"
